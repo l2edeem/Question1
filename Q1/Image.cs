3f5926bf43847b8d3c9bc7869f2c985547ad3e8e
Ougrid339 <ougrid3top@gmail.com>
2022-04-07 16:31:16 +0700

    First commit

---
+++ Q1/Image.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    public class Image
+    {
+        public string Getstar()
+        {
+            return "*";
+        }
+        public void PrintImage(int count,string star)
+        {
+            int i, j, k, z;
+            count = count - 2;
+            for (i = 0; i <= count; i++)
+            {
+                for (j = count; j >= 0; j--)
+                {
+                    if (i < j)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write(star);
+                    }
+                }
+                for (k = 0; k < i; k++)
+                {
+                    Console.Write(star);
+                }
+                Console.WriteLine();
+            }
+            for (i = 1; i <= 2; i++)
+            {
+                for (z = 0; z < count; z++)
+                {
+                    Console.Write(" ");
+                }
+                count--;
+                if (i == 1)
+                {
+                    Console.WriteLine(star);
+                }
+                else
+                {
+                    Console.WriteLine("***");
+                }
+
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Q1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Image image = new Image();
+            var star = image.Getstar();
+            Console.Write("Input :Number more than 4 ");
+            var input = Console.ReadLine();
+            int count = Convert.ToInt32(input);
+            Console.WriteLine("Output:");
+            Console.WriteLine();
+            image.PrintImage(count, star);
+        }
+    }
+}
